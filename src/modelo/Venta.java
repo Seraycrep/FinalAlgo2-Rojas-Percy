77e310f915acfd72ffe53c3f30d86abb817fd21f
Usuario <Usuario@192.168.0.109>
2023-07-14 21:25:38 -0500

    Implementados los métodos agregarProducto, eliminarProducto y validarCarrito , de la clase Venta.

--- src/modelo/Venta.java
+++ src/modelo/Venta.java
@@ -9,7 +9,9 @@ public class Venta {
     private boolean estado;
     private Trabajador trabajador;
     private Cliente cliente;
+    private int contDetalles;
     private Detalle detalles[];
+    private static final int CAPACIDAD_INICIAL = 10;
 
     public Venta(float total, Date fecha, boolean estado, Trabajador trabajador, Cliente cliente, Detalle[] detalles) {
         this.total = total;
@@ -17,7 +19,8 @@ public class Venta {
         this.estado = estado;
         this.trabajador = trabajador;
         this.cliente = cliente;
-        this.detalles = detalles;
+        this.detalles = new Detalle[CAPACIDAD_INICIAL];
+        this.contDetalles = 0;
     }
 
     public float getTotal() {
@@ -61,15 +64,51 @@ public class Venta {
     }
     
     public void agregarProducto(Producto p, int cantidad, float descuento) {
-        //Detalle detalle = new Detalle(cantidad, p.getPrecio(), descuento, this);
+        float precio = p.getPrecio();
+        float total = precio * cantidad * (1-descuento);
+        Detalle detalle = new Detalle(cantidad, precio, descuento, total, p);
+        detalles[contDetalles] = detalle;
+        contDetalles++;
     }
     
     public boolean eliminarProducto(Producto p, int cantidad) {
+        int indice = encontrarIndiceProducto(p);
+        if(indice != -1) {
+            Detalle detalle = detalles[indice];
+            int cantidadActual = detalle.getCantidad();
+            
+            if(cantidadActual <= cantidad) {
+                for(int i=indice; i < contDetalles-1; i++) {
+                    detalles[i] = detalles[i+1];
+                }
+                detalles[contDetalles-1] = null;
+                contDetalles--;
+                return true;
+            }
+            else {
+                detalle.setCantidad(cantidadActual-cantidad);
+                return true;
+            }
+        }
         return false;
     }
     
-    public float validarCarrito() {
+    private int encontrarIndiceProducto(Producto p) {
+        for(int i=0; i < contDetalles; i++) {
+            if(detalles[i].getProducto().equals(p)) {
+                return i;
+            }
+        }
         
-        return 0;  
+        return -1;
+    }
+    
+    public float validarCarrito() {
+        float precioTotal = 0.0f;
+        for(int i=0; i < contDetalles; i++) {
+            Detalle detalle = detalles[i];
+            precioTotal += detalle.getTotal();
+        }
+        return precioTotal;  
     }
 }
